cc26c70fd78a3ac3b9288ae60eed5e91fd194e38
Lars Jorgensen <larsgt@new-host-3.home>
2012-05-03 09:25:17 -0400

    First version

---
+++ app/controllers/forum_controller.rb
@@ -0,0 +1,5 @@
+class ForumController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medspace2::Application.config.secret_token = 'd6a851c0c355527517e4a9097b42afef0d4ae8e2f30923d69c984fe3c693d12c94b11d41bf8017c423e64a833de6d2a7da815ece7ad1a04727c870b35e7711dc'
